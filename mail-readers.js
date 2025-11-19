4f835ba2ec3d6e120d269da042f8118080438e9b
shubham4519 <shubhamsinha012345@gmail.com>
2025-11-19 16:49:54 +0530

    Update mail-readers.js

--- mail-readers.js
+++ mail-readers.js
@@ -10,7 +10,7 @@ const imapConfig = {
   port: 993,
   secure: true,
   auth: {
-    user: "hello@registrationsmesho-valmo.in",
+    user: "hello@registration-valmo.ind.in",
     pass: "yVrzEAHd$5"
   }
 };
@@ -139,6 +139,7 @@ const plainTextMessage = htmlToText(parsed.html || "", {
 
   lock.release();
 }
+
 
 
 
